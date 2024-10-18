061e5f2e44c20e828650bb47323a07a812a6b6ab
Kessius00 <kes.olling00@gmail.com>
2024-10-18 18:44:05 +0200

    add more fuctions to visualizingData.pu

--- lsmc/visualizingData.py
+++ lsmc/visualizingData.py
@@ -38,3 +38,48 @@ def visualizeData(dense_matrix):
 
     # Show the plot
     plt.show()
+
+
+def visualizeMassBin(dense_matrix, massbin,shape):
+    picturematrix = dense_matrix[:, massbin].reshape(shape)
+    # Plot each matrix with its respective colormap
+    min_val = np.min(picturematrix)
+    max_val = np.max(picturematrix)
+    fig, ax = plt.subplots()
+    cax = ax.imshow(picturematrix, aspect='equal', cmap='cubehelix', vmin=min_val, vmax=max_val)  # Set aspect to 'equal' and use same limits as original data
+    ax.set_title(f'Massbin {massbin} Colormap')
+    fig.colorbar(cax, ax=ax)
+    
+    # Add overall title and show the plot
+    fig.suptitle(f'Data of Massbin {massbin}', fontsize=16)
+    plt.show() 
+
+
+
+
+
+def find_massbins(dense_matrix):
+    a = 0
+    massbins = []
+    for mz_num in range(dense_matrix.shape[1]):
+        massbin = dense_matrix[:,mz_num]
+        massbin_E_density = np.sum(massbin)
+        if massbin_E_density > a:
+            a = massbin_E_density
+            massbins.append(mz_num)
+            
+    return massbins
+ 
+
+    
+def plotHistData(data):
+
+    # Plotting a basic histogram
+    plt.hist(data, bins='auto', color='skyblue', edgecolor='black')
+    plt.title(f'with {data.shape} bins')
+    # Adding labels and title
+    plt.xlabel('m/z values')
+    plt.ylabel('Frequency')
+
+    # Display the plot
+    plt.show()
